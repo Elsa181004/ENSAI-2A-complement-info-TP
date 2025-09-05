eda6e7cbe4e415f5a73c1d660df661c85a0eb252
id2743 <elsa.laviec@eleve.ensai.fr>
2025-09-05 10:47:16 +0000

    m

--- src/business_object/pokemon/fixed_damage_attack.py
+++ src/business_object/pokemon/fixed_damage_attack.py
@@ -1,8 +1,6 @@
 from business_object.pokemon.abstract_attack import AbstractAttack
 
 class FixedDamageAttack(AbstractAttack):
-    def __init__(self):
-        super().__init__()
 
     def compute_damage(self, pok_1, pok_2):
         return self._power 
--- src/tests/test_business_object/test_pokemon/test_fixed_damage_attack.py
+++ src/tests/test_business_object/test_pokemon/test_fixed_damage_attack.py
@@ -1,17 +1,10 @@
 from business_object.pokemon.fixed_damage_attack import FixedDamageAttack
+
 class TestFixedDamageAttack:
      def test_compute_damage(self):
         # GIVEN
-        attack = FixedDamageAttack(power=10)
+        attack = FixedDamageAttack(power=10, "")
 
-        # WHEN
-        
 
         # THEN
         assert attack.compute_damage() == 10
-
-
-if __name__ == "__main__":
-    import pytest
-
-    pytest.main([__file__])
